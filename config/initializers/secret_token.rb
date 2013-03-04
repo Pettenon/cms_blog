9eb926be6497cf7d60b0c2ca1ab4dafa81616f24
VictorPettenon <pettenon.r3cotia@gmail.com>
2013-03-04 12:43:43 -0300

    Refinery CMS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piano::Application.config.secret_token = '5b517ee55c621f5921c05d2942a1c9aee5b3c8abe7f607c2781c0bde8bd2dd40b7fcfc2c78c9580838f208749dddb510818f1b899e422a8029c5a5c2a05f8805'
